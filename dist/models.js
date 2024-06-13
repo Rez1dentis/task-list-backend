5962967f6a7a124859c9af1e71f5deeaab785e09
Jean <jean.kosenko@gmail.com>
2024-06-13 17:40:26 +0300

    new config for deploy

---
+++ dist/models.js
@@ -0,0 +1,40 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CTask = void 0;
+const sequelize_1 = require("sequelize");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+});
+class CTask extends sequelize_1.Model {
+}
+exports.CTask = CTask;
+CTask.init({
+    id: {
+        type: sequelize_1.DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: new sequelize_1.DataTypes.STRING(128),
+        allowNull: false,
+    },
+    isCompleted: {
+        type: sequelize_1.DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    date: {
+        type: sequelize_1.DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    tableName: 'tasks',
+    sequelize,
+});
+sequelize.sync().then(() => console.log('База данных и таблица созданы!'));
+//# sourceMappingURL=models.js.map
